fb41ba35b0f81bcd671b40f7ff0c27324e6ebc32
Lionel Henry <lionel.hry@proton.me>
2026-04-24 08:38:47 +0200

    Log caught panics in release builds

--- crates/ark/src/console/console_repl.rs
+++ crates/ark/src/console/console_repl.rs
@@ -699,6 +699,7 @@ impl Console {
                         None => String::from("(unknown payload)"),
                     },
                 };
+                log::error!("Panic in Console callback: {msg}");
                 Err(anyhow!("Panic in Console callback: {msg}"))
             },
         }
